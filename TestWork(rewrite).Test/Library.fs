0cdbfff7b3bd0e2d288889ff7a5c661a9fad3577
MaxVortman <maka.abort@gmail.com>
2018-04-24 11:21:17 +0300

    Time....

--- TestWork(rewrite).Test/Library.fs
+++ TestWork(rewrite).Test/Library.fs
@@ -5,5 +5,17 @@ open FsUnit
 open TestWork_rewrite_
 
 module ``1`` =
+    open TestWork_rewrite_.``1``
+
     [<Test>]
-    let ``say hello Max``() = "Max" |> Say.hello |> should equal "Hello Max"
+    let ``should be [sin 1, cos 1, sin 2, cos 2, sin 3, cos 3]``() =   (psevdoMap [1.; 2.; 3.] [sin; cos])  |> should equal [0.8414709848078965; 0.90929742682568171; 0.14112000805986721; 0.54030230586813977; -0.41614683654714241; -0.98999249660044542]
+
+
+
+module ``2`` =
+    open TestWork_rewrite_.``2``
+
+    [<Test>]
+    let ``should print square  with length 4``() = 
+        let square = "****\n*  *\n*  *\n****\n"
+        4 |> printSquare |> should equal square
--- TestWork(rewrite)/Library.fs
+++ TestWork(rewrite)/Library.fs
@@ -1,5 +1,62 @@
 namespace TestWork_rewrite_
 
-module Say =
-    let hello name =
-         "Hello " + name
+module ``1`` =
+    
+    let psevdoMap l lf =
+        let f h = List.map h l
+        let rec map acc list = 
+            match list with
+            | h :: t -> map (acc @ (f h)) t
+            | _ -> acc
+        map [] lf
+
+module ``2`` = 
+    
+    let printSquare length = 
+        let print = "*" 
+        let newLine = "\n"
+        let backspace = " "
+        let (^) l r = sprintf "%s%s" l r
+        let rec printS acc x y =
+            
+            match x with
+            | _ when x < length && (y = 0 || y = length - 1) -> printS (acc ^ print) (x + 1) y
+            | _ when x = length -> printS (acc ^ newLine) 0 (y + 1)
+            | _ when x = 0 && y > 0 && y < length -> printS (acc ^ print) (x + 1) y
+            | _ when x < length - 1 && y > 0 && y < length -> printS (acc ^ backspace) (x + 1) y
+            | _ when x = length - 1 && y < length -> printS (acc ^ print) (x + 1) y
+            | _ -> acc
+
+        printS "" 0 0
+
+module ``3`` =
+    open System.Collections.Generic
+    open System.Linq
+
+    [<AllowNullLiteral>]
+    type Linked<'a>(value) =
+        
+        member val Value : 'a = value with get, set
+
+        member val Next : Linked<'a> = null with get, set
+        
+        
+
+
+    type Queue<'a>() =
+        let queue : Dictionary<int, Linked<'a>> = new Dictionary<int, Linked<'a>>()
+
+        let highPriority : int = queue.Keys.Max()        
+
+        let getTail (elem : Linked<'a>) =
+            let rec get tail = if elem.Next <> null then get elem.Next else tail 
+            get elem
+        
+
+        member this.Enqueue (elem : 'a) priority = if queue.ContainsKey(priority) then (getTail queue.[priority]).Next <- new Linked<'a>(elem) 
+                                                   else queue.Add(priority, new Linked<'a>(elem))
+
+        member this.Dequeue = 
+            let elem = getTail queue.[highPriority]
+            //time....
+            
