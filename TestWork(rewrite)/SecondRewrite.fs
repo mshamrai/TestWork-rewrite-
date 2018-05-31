c2c00d8784b18ac397891cfd2013a5f23715de8c
MaxVortman <maka.abort@gmail.com>
2018-05-31 12:05:23 +0300

    3

--- TestWork(rewrite)/SecondRewrite.fs
+++ TestWork(rewrite)/SecondRewrite.fs
@@ -12,3 +12,18 @@ module ``1`` =
             fibSeq 0I 1I
         infiniteFibSeq |> Seq.takeWhile (fun x -> x < 1000000I) |> Seq.where (fun x -> x % 2I = 0I) |> Seq.sum
 
+module ``3`` =
+    open System
+    type Stack<'a>() =
+        let mutable elems = []
+        member this.Push (elem : 'a) = 
+            elems <- elem :: elems
+        member this.Pop =
+            match elems with
+            | h :: t -> elems <- t
+                        h
+            | [] -> raise (InvalidOperationException())
+        member this.IsEmpty = 
+            match elems with
+            | [] -> true
+            | _ -> false
