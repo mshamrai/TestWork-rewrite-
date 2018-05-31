a9988f3753fe3c95cf9ec5aab8c98598df554c75
MaxVortman <maka.abort@gmail.com>
2018-05-31 11:42:48 +0300

    1

---
+++ TestWork(rewrite).Test/SecondRewriteTest.fs
@@ -0,0 +1,10 @@
+﻿namespace SecondRewriteTest.Test
+
+open NUnit.Framework
+open FsUnit
+
+module ``1`` = 
+    open SecondRewrite.``1``
+    [<Test>]
+    let sumOfEvenFibLessOfBillion () = 
+        sumOfLessBillionEvenFib |> should equal 2178308I
---
+++ TestWork(rewrite)/SecondRewrite.fs
@@ -0,0 +1,14 @@
+﻿namespace SecondRewrite
+
+module ``1`` =
+    let sumOfLessBillionEvenFib = 
+        let infiniteFibSeq = 
+            let nextFib prevFib prevprevFib = 
+                prevFib + prevprevFib
+            let rec fibSeq prevNumber number = 
+                seq { yield prevNumber
+                      yield number
+                      yield! nextFib number prevNumber |> fibSeq number }
+            fibSeq 0I 1I
+        infiniteFibSeq |> Seq.takeWhile (fun x -> x < 1000000I) |> Seq.where (fun x -> x % 2I = 0I) |> Seq.sum
+
